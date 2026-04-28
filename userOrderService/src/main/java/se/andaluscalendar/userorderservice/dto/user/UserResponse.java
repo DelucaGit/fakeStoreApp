03a6e908625e9033b1783475d25ac774eb7357aa
Marcel Deluca <andalusi.private@gmail.com>
2026-04-28 15:54:37 +0200

    Create JWT when registering a new user and return token with UserResponse DTO

--- userOrderService/src/main/java/se/andaluscalendar/userorderservice/dto/user/UserResponse.java
+++ userOrderService/src/main/java/se/andaluscalendar/userorderservice/dto/user/UserResponse.java
@@ -1,9 +1,16 @@
 package se.andaluscalendar.userorderservice.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 // UserResponse is used both when registering a new user and when fetching data from a current user
+/* JsonInclude makes sure that NULL variables are not sent in the response. This is because this
+* DTO is used both when registering and fetching an user. So the JWT token will sometimes be filled and sometimes
+* NULL.
+* */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserResponse(
         UUID id,
         String email,
